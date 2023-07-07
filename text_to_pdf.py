41b4d89a1ee074c6f9716a1f44ca1465f4511764
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-07 10:52:54 -0700

    daily exercise problem finished

--- text_to_pdf.py
+++ text_to_pdf.py
@@ -10,12 +10,18 @@ for filepath in filepaths:
     pdf.add_page()
     filename = Path(filepath).stem
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=filename)
+    pdf.cell(w=50, h=8, txt=filename.title(), border=0)
 
     with open(filepath, "r") as file:
-        data = file.read()
+        data = file.readlines()
     pdf.ln(10)
     pdf.set_font(family="Times", size=11)
-    pdf.cell(w=50, h=8, txt=data)
-    pdf.output(f"txt_pdf/{filename}.pdf")
 
+    count = 8
+    for line in data:
+        pdf.cell(w=170, h=12, txt=line, border=0)
+        pdf.ln(12)
+    # pdf.output(f"txt_pdf/{filename}.pdf")
+
+# produces one pdf with multiple pages
+pdf.output("animals.pdf")
