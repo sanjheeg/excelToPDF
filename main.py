48f888c6efc669114476ddba1b65506d12a0e036
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-07 10:05:16 -0700

    create pdf, title, and store output

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 # .glob creates a list of all the filepaths of all files that end with .xlsx
 filepaths = glob.glob("invoices/*.xlsx")
 
+# creates the pdf, its title, and stores output in directory
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    # Path(filepath) --> PosixPath("invoices/10001-2023.1.18.xlsx")
+    # Path(filepath).stem = "10001-2023.1.18"
+    filename = Path(filepath).stem
+    inv_num = filename.split("-")[0]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{inv_num}")
+    pdf.output(f"PDFs/{filename}.pdf")
