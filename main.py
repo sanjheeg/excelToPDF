576b0482e31fda80051f9bc05011c4f5d6543c0c
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-10 10:01:22 -0700

    data

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 # creates the pdf, its title, and stores output in directory
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
@@ -24,6 +22,18 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{inv_num}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{date}")
+    pdf.cell(w=50, h=8, txt=f"date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt =str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
 
     pdf.output(f"PDFs/{filename}.pdf")
