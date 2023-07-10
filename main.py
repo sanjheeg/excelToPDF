76fdc665363a804d324a763096e25e71bbeed416
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-10 09:43:37 -0700

    add date

--- main.py
+++ main.py
@@ -9,12 +9,21 @@ filepaths = glob.glob("invoices/*.xlsx")
 # creates the pdf, its title, and stores output in directory
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     # Path(filepath) --> PosixPath("invoices/10001-2023.1.18.xlsx")
     # Path(filepath).stem = "10001-2023.1.18"
     filename = Path(filepath).stem
+
     inv_num = filename.split("-")[0]
+    date = filename.split("-")[1]
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{inv_num}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{inv_num}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{date}")
+
     pdf.output(f"PDFs/{filename}.pdf")
